e8c243d72270b1e4f4477098bafa48a2a526721c
aaomerali <aaomerali@gmail.com>
2024-09-14 16:15:58 +0300

    completed app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import logo from './logo.png';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { useState } from 'react';
 import './App.css';
 
+//require('dotenv').config()
+
 function App() {
+
+  const [skill , setSkill] = useState();
+  const [roadmap , setRoadmap] = useState();
+  const [generatingAnswer, setGeneratingAnswer] = useState(false);
+
+  async function generateRoadmap(e) {
+    setGeneratingAnswer(true);
+    e.preventDefault();
+
+    //setRoadmap("Loading your skill path... \n It might take upto 10 seconds");
+
+    try {
+
+      const genAi = new GoogleGenerativeAI(process.env.REACT_APP_API_KEY)
+      const model = genAi.getGenerativeModel({model : 'gemini-1.5-flash'})
+
+      const prompt= `write a proffissonal roadmap to learn ${skill} skill and make it as apoints without details , also use emojies`
+
+      const result = await model.generateContent(prompt)
+      setRoadmap(result.response.text())
+
+      setGeneratingAnswer(false);
+
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="container">
+
+        <img className="logo" src={logo} alt="logo"></img>
+
+        <div className="skill-container">
+          <label htmlFor="skill">Write the skill you want to gain:</label>
+          <input type="text" value={skill} onChange={(e) => setSkill(e.target.value)} placeholder='Ex: Graphic design, Web development, project management..' />
+        </div>
+
+        
+        <button className='generator-btn' onClick={generateRoadmap}>Submit</button>
+
+        <div className="roadmap-container">
+          {generatingAnswer && (
+            <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+          )}
+          <pre className='roadmap'>{roadmap}</pre>
+        </div>
+
+
+        <p className='madeBy'>@ Made by: Abdulrahman Omar</p>
+
+      </div>
+      
     </div>
   );
 }
